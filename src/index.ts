4cb3e633e55c6b4d1e1ddacddeca179403b358c1
Antoine Kingue <etorefr@gmail.com>
2022-10-23 02:26:18 +0200

    🎨 add multiples entrypoints

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
-import "./styled-components";
+import "./types/styled-components";
 
 export * from "./components/Link";
+export * from "./components/Provider";
--- src/types/styled-components.ts
+++ src/types/styled-components.ts
@@ -1,5 +1,5 @@
 import "styled-components";
-import { MainTheme, Theme } from "./types/theme";
+import { MainTheme, Theme } from "./theme";
 
 declare module "styled-components" {
   export interface DefaultTheme extends Theme, MainTheme {}
