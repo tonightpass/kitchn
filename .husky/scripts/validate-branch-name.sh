799d1503a98792d2ed8debbbdd4adf78f421c432
Jérémy <work@jeremybdn.fr>
2022-10-24 15:15:11 +0200

    ➕ update size-limit dependency for small libs to big libs

---
+++ .husky/scripts/validate-branch-name.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+branchName="$(git rev-parse --abbrev-ref HEAD)"
+regex="^([a-z0-9]+)\/([a-z-]+)$"
+
+if [[ ! $branchName =~ $regex ]];
+then
+  echo " "
+  echo "🚨 Invalid branch name!"
+  echo " "
+  exit 1
+else
+  echo " "
+  echo "✔️ Valid branch name!"
+  echo " "
+fi
