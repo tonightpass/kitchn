ffb0d60738545703b0e82590593cbf6d1653fb6a
Antoine Kingue <etorefr@gmail.com>
2022-11-11 22:47:28 +0100

    ✨ add text

--- packages/kitchen/src/index.ts
+++ packages/kitchen/src/index.ts
@@ -4,10 +4,19 @@ import kitchen from "styled-components";
 import KitchenProvider from "./components/Provider";
 import KitchenDocument from "./components/Document";
 import Button from "./components/Button";
+import Text from "./components/Text";
 import Icon from "./components/Icon";
 import Spinner from "./components/Spinner";
 
 import withKitchen from "./hoc/withKitchen";
 
-export { KitchenProvider, KitchenDocument, Button, Icon, Spinner, withKitchen };
+export {
+  KitchenProvider,
+  KitchenDocument,
+  Button,
+  Text,
+  Icon,
+  Spinner,
+  withKitchen,
+};
 export default kitchen;
