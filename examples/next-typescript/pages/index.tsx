3206a324c7ab389107089a417ad5cd0c671ca9ec
Antoine Kingue <etorefr@gmail.com>
2022-11-14 03:56:23 +0100

    ✨ add grid

--- examples/next-typescript/pages/index.tsx
+++ examples/next-typescript/pages/index.tsx
@@ -3,10 +3,6 @@ import Link from "next/link";
 import React from "react";
 
 const IndexPage: NextPage = () => {
-  const ref = React.useRef(null);
-
-  console.log("ref", ref);
-
   return (
     <div
       style={{
@@ -18,9 +14,8 @@ const IndexPage: NextPage = () => {
         gap: "10px",
       }}
     >
-      <Link ref={ref} href={"/button"}>
-        Button
-      </Link>
+      <Link href={"/grid"}>Grid</Link>
+      <Link href={"/button"}>Button</Link>
       <Link href={"/text"}>Text</Link>
     </div>
   );
---
+++ packages/kitchen/src/components/Container/index.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import styled from "styled-components";
+import { KitchenComponent } from "../../types";
+
+type ContainerProps = KitchenComponent & {
+  row?: boolean;
+  flex?: number;
+  direction?: ("row" | "column")[];
+  gap?: number;
+  align?: "center" | "flex-start" | "flex-end" | "stretch" | "baseline";
+  justify?:
+    | "center"
+    | "flex-start"
+    | "flex-end"
+    | "space-between"
+    | "space-around"
+    | "space-evenly"
+    | "stretch";
+};
+
+const Container = styled(({ children, ...rest }: ContainerProps) => {
+  return <div {...rest}>{children}</div>;
+})`
+  display: flex;
+  flex-direction: ${(props) => (props.row ? "row" : "column")};
+  position: relative;
+  max-width: 100%;
+  flex: ${(props) => props.flex || 1};
+  justify-content: ${(props) => props.justify || "flex-start"};
+  align-items: ${(props) => props.align || "stretch"};
+  ${(props) => props.gap && `gap: ${props.gap}px;`}
+
+  @media (max-width: ${(props) => props.theme.breakpoint.laptop}) {
+    ${(props) =>
+      props.direction &&
+      props.direction[2] &&
+      `flex-direction: ${props.direction[2]};`}
+  }
+
+  @media (max-width: ${(props) => props.theme.breakpoint.tablet}) {
+    ${(props) =>
+      props.direction &&
+      props.direction[1] &&
+      `flex-direction: ${props.direction[1]};`}
+  }
+
+  @media (max-width: ${(props) => props.theme.breakpoint.mobile}) {
+    ${(props) =>
+      props.direction &&
+      props.direction[0] &&
+      `flex-direction: ${props.direction[0]};`}
+  }
+`;
+
+export default Container;
