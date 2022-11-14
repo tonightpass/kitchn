38aaba569f267d7d6976516a6146532c1f64cbbf
Antoine Kingue <etorefr@gmail.com>
2022-11-14 15:29:03 +0100

    ✨ add icon

--- packages/kitchen/src/components/Icon/index.tsx
+++ packages/kitchen/src/components/Icon/index.tsx
@@ -1,33 +1,39 @@
-import styled from "styled-components";
-//import "remixicon/fonts/remixicon.css";
+import React from "react";
+import styled, { useTheme } from "styled-components";
+import { KitchenComponent } from "../../types";
+import { IconType, IconBaseProps } from "react-icons";
+import { AccentColors, Size, TextColors } from "../../types/theme";
 
-interface Props
-  extends Omit<
-    React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>,
-    never
-  > {
-  align?: "top" | "middle" | "bottom";
-  size?: number;
-  name: string;
-  fill?: boolean;
-  simple?: boolean;
-  color?: string;
-  className?: string;
-}
+export type IconProps = KitchenComponent &
+  IconBaseProps & {
+    icon: IconType;
+    size?: keyof Size | number | string;
+    /**
+     * The text color. Strictly limited to colors of our design system. If you want to pass accent color make sure to pass `accent` instead of `color`.
+     */
+    color?: keyof TextColors;
+    /**
+     * The accent color. Strictly limited to colors of our design system, but can be used in combination with `color` prop.
+     */
+    accent?: keyof AccentColors;
+    align?: "top" | "middle" | "bottom";
+  };
 
-/**
- * @description
- * This is a styled component that renders an icon from remixicon.
- */
-const Icon = styled(
-  ({ name, fill, simple = false, className, ...rest }: Props) => {
-    const finalName = `ri-${name}${!simple ? (fill ? "-fill" : "-line") : ""}`;
-    const finalClassName = `${finalName} ${className || ""}`;
-    return <i {...rest} className={finalClassName} />;
-  }
-)<Props>`
-  font-size: ${({ size, theme }) => (size ? `${size}px` : theme.size.normal)};
-  ${({ color }) => color && `color: ${color}`};
+const Icon = styled(({ icon: Component, size, ...props }) => {
+  const theme = useTheme();
+  return (
+    <Component
+      size={theme.size[size as keyof Size] || size || theme.size.normal}
+      {...props}
+    />
+  );
+})<IconProps>`
+  color: ${(props) =>
+    props.theme.colors.accent[props.accent as keyof AccentColors] ||
+    props.theme.colors.text[props.color as keyof TextColors] ||
+    props.color ||
+    props.theme.colors.text.lightest};
+  };
   vertical-align: ${({ align }) => {
     switch (align) {
       case "top":
