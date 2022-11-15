4400481cb2734c2c02ddc9252ab7d1ac05ec22d8
Antoine Kingue <etorefr@gmail.com>
2022-11-15 20:29:08 +0100

    🔧 remove limits from size limit config

--- .size-limit.js
+++ .size-limit.js
@@ -16,32 +16,26 @@ const modifyWebpackConfig = (config) => {
 module.exports = [
   {
     path: "packages/kitchen/dist/tonightpass-kitchen.cjs.prod.js",
-    limit: "1.25 s",
     modifyWebpackConfig,
   },
   {
     path: "packages/kitchen/dist/tonightpass-kitchen.esm.js",
-    limit: "1.25 s",
     modifyWebpackConfig,
   },
   {
     path: "packages/kitchen/next/dist/tonightpass-kitchen-next.cjs.prod.js",
-    limit: "1 s",
     modifyWebpackConfig,
   },
   {
     path: "packages/kitchen/next/dist/tonightpass-kitchen-next.esm.js",
-    limit: "1 s",
     modifyWebpackConfig,
   },
   {
     path: "packages/kitchen/native/dist/tonightpass-kitchen-native.cjs.prod.js",
-    limit: "1.25 s",
     modifyWebpackConfig,
   },
   {
     path: "packages/kitchen/native/dist/tonightpass-kitchen-native.esm.js",
-    limit: "1.25 s",
     modifyWebpackConfig,
   },
 ];
