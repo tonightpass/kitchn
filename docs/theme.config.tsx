279dabc5c56c3e9d6fde354aac9297f5bf5182d9
Antoine Kingue <etorefr@gmail.com>
2022-12-01 00:09:52 +0100

    📝 update docs

--- docs/theme.config.tsx
+++ docs/theme.config.tsx
@@ -22,23 +22,6 @@ const logo = (
         strokeWidth="2"
       />
     </svg>
-    <style>{`
-      span {
-        padding: 0.5rem 0.5rem 0.5rem 0;
-        mask-image: linear-gradient(
-          60deg,
-          black 25%,
-          rgba(0, 0, 0, 0.2) 50%,
-          black 75%
-        );
-        mask-size: 400%;
-        mask-position: 0%;
-      }
-      span:hover {
-        mask-position: 100%;
-        transition: mask-position 1s ease, -webkit-mask-position 1s ease;
-      }
-    `}</style>
   </span>
 );
 
