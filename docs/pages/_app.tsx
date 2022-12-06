39acf1bd59dcd23cbd90d5db14148ec93306bd0c
Antoine Kingue <etorefr@gmail.com>
2022-12-06 06:53:09 +0100

    🎨 improve custom stylesheet

--- docs/pages/_app.tsx
+++ docs/pages/_app.tsx
@@ -1,11 +1,11 @@
 import { KitchenProvider } from "@tonightpass/kitchen";
 import { AppProps } from "next/app";
-
-import "../styles/custom.css";
+import GlobalStyle from "@components/GlobalStyle";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <KitchenProvider>
+      <GlobalStyle />
       <Component {...pageProps} />
     </KitchenProvider>
   );
