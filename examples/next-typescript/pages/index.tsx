9d4cd56c23b65ff960946c3d83173a4aa2ed168e
Antoine Kingue <etorefr@gmail.com>
2022-12-07 06:51:45 +0100

    🎨 improve example with theme detector

--- examples/next-typescript/pages/index.tsx
+++ examples/next-typescript/pages/index.tsx
@@ -1,4 +1,11 @@
-import { Button, Container, themes, useTheme } from "@tonightpass/kitchen";
+import {
+  Button,
+  Container,
+  Text,
+  themes,
+  useTheme,
+  useThemeDetector,
+} from "@tonightpass/kitchen";
 import { NextPage } from "next";
 import Link from "next/link";
 import React from "react";
@@ -6,8 +13,11 @@ import React from "react";
 const IndexPage: NextPage = () => {
   const { setTheme } = useTheme();
 
+  const isDarkTheme = useThemeDetector();
+
   return (
-    <Container align="center" gap={10} style={{ marginTop: 100 }}>
+    <Container align="center" gap={10} style={{ paddingTop: 100 }}>
+      <Text>Your system theme is: {isDarkTheme ? "dark" : "light"}</Text>
       <Container gap={10} row>
         <Button type={"light"} onClick={() => setTheme(themes.light)}>
           Light
