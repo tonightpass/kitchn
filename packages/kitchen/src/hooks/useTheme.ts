eedc492dacd12e3d72a9b86d4da841e6deecf986
Antoine Kingue <etorefr@gmail.com>
2022-12-07 20:23:36 +0100

    🎨 improve theme hook with default themes

--- packages/kitchen/src/hooks/useTheme.ts
+++ packages/kitchen/src/hooks/useTheme.ts
@@ -1,10 +1,11 @@
 import React from "react";
 import { ThemeContext } from "../contexts/Theme";
+import themes from "../themes";
 
 const useTheme = () => {
   const context = React.useContext(ThemeContext);
 
-  return context;
+  return { ...context, defaultThemes: themes };
 };
 
 export default useTheme;
