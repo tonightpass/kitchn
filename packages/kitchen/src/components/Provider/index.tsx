82c056f99e2e8a569fd35aed8f04af22b2cbfbe9
Antoine Kingue <etorefr@gmail.com>
2022-12-07 06:53:01 +0100

    🩹 remove default theme given by the provider, that override the theme detection

--- packages/kitchen/src/components/Provider/index.tsx
+++ packages/kitchen/src/components/Provider/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { DefaultTheme } from "styled-components";
 import { ThemeProvider } from "../../contexts/Theme";
-import themes from "../../themes";
 import GlobalStyle from "../GlobalStyle";
 
 export type KitchenProviderProps = {
@@ -11,7 +10,7 @@ export type KitchenProviderProps = {
 
 const KitchenProvider: React.FC<KitchenProviderProps> = ({
   children,
-  theme = themes.dark,
+  theme,
 }: KitchenProviderProps) => {
   return (
     <ThemeProvider theme={theme}>
