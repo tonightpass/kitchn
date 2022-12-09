2e88c8be02dbf390931fe162a554f942760369db
Antoine Kingue <etorefr@gmail.com>
2022-12-09 03:59:04 +0100

    ✨ add ssr hook

---
+++ packages/kitchen/src/hooks/useSSR.ts
@@ -0,0 +1,21 @@
+import React from "react";
+import isBrowser from "../utils/isBrowser";
+
+export type SSRState = {
+  isBrowser: boolean;
+  isServer: boolean;
+};
+
+const useSSR = (): SSRState => {
+  const [browser, setBrowser] = React.useState<boolean>(false);
+  React.useEffect(() => {
+    setBrowser(isBrowser());
+  }, []);
+
+  return {
+    isBrowser: browser,
+    isServer: !browser,
+  };
+};
+
+export default useSSR;
