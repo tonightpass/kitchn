5cbefc88c132b8517249f05d85b4459e02e490c3
Shivam Sharma <shivamcod2@gmail.com>
2022-12-09 20:51:26 +0530

    fix editor colors

--- docs/components/Playground/code-theme.ts
+++ docs/components/Playground/code-theme.ts
@@ -58,7 +58,7 @@ const makeCodeTheme = (theme: DefaultTheme): PrismTheme => ({
       types: ["attr-name"],
       style: {
         color: "#FAD000",
-      },
+      }
     },
     {
       types: [
