d07f1999c2732ce9de9330e82b795961ebb06b5f
Antoine Kingue <etorefr@gmail.com>
2022-12-19 13:42:17 +0100

    📝 update checkbox example

--- examples/next-typescript/pages/checkbox.tsx
+++ examples/next-typescript/pages/checkbox.tsx
@@ -4,111 +4,49 @@ import React from "react";
 
 const CheckboxPage: NextPage = () => {
   return (
-    <>
-      <Container
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-          height: "100vh",
-          gap: "10px",
-        }}
-      >
-        <Container
-          gap={10}
-          style={{
-            maxWidth: 700,
-            margin: "0 auto",
-            marginTop: "15px",
-            alignItems: "center",
-          }}
-        >
-          <p>default</p>
-          <Checkbox>Default</Checkbox>
-        </Container>
-        <Container
-          gap={10}
-          style={{
-            maxWidth: 700,
-            margin: "0 auto",
-            marginTop: "15px",
-            alignItems: "center",
-          }}
-        >
-          <p>checked</p>
-          <Checkbox checked>Checked</Checkbox>
-        </Container>
-        <Container
-          gap={10}
-          style={{
-            maxWidth: 700,
-            margin: "0 auto",
-            marginTop: "15px",
-            alignItems: "center",
-          }}
-        >
-          <p>no label</p>
-          <Checkbox />
-        </Container>
-        <Container
-          gap={10}
-          style={{
-            maxWidth: 700,
-            margin: "0 auto",
-            marginTop: "15px",
-            alignItems: "center",
-          }}
-        >
-          <p>disabled</p>
-          <Checkbox disabled>Disabled</Checkbox>
-        </Container>
-        <Container
-          gap={10}
-          style={{
-            maxWidth: 700,
-            margin: "0 auto",
-            marginTop: "15px",
-            alignItems: "center",
-          }}
-        >
-          <p>indeterminate</p>
-          <Checkbox indeterminate>Indeterminate</Checkbox>
-        </Container>
-        <Container
-          gap={10}
-          style={{
-            maxWidth: 700,
-            margin: "0 auto",
-            marginTop: "15px",
-            alignItems: "center",
-          }}
-        >
-          <p>overflowing text</p>
-          <Checkbox>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum
-          </Checkbox>
-        </Container>
-        <Container
-          gap={10}
-          style={{
-            maxWidth: 700,
-            margin: "0 auto",
-            marginTop: "15px",
-            alignItems: "center",
-          }}
-        >
-          <p>full width</p>
-          <Checkbox fullWidth>Full Width</Checkbox>
-        </Container>
+    <Container
+      style={{
+        maxWidth: 700,
+        margin: "0 auto",
+      }}
+    >
+      <Container gap={10}>
+        <p>default</p>
+        <Checkbox>Default</Checkbox>
       </Container>
-    </>
+      <Container gap={10}>
+        <p>checked</p>
+        <Checkbox checked>Checked</Checkbox>
+      </Container>
+      <Container gap={10}>
+        <p>no label</p>
+        <Checkbox />
+      </Container>
+      <Container gap={10}>
+        <p>disabled</p>
+        <Checkbox disabled>Disabled</Checkbox>
+      </Container>
+      <Container gap={10}>
+        <p>indeterminate</p>
+        <Checkbox indeterminate>Indeterminate</Checkbox>
+      </Container>
+      <Container gap={10}>
+        <p>overflowing text</p>
+        <Checkbox>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum
+        </Checkbox>
+      </Container>
+      <Container gap={10}>
+        <p>full width</p>
+        <Checkbox fullWidth>Full Width</Checkbox>
+      </Container>
+    </Container>
   );
 };
 
