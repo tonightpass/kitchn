c9d86a50ca00fb2b39f78339bf527e6e11283053
Younes Bessa <bessa460@gmail.com>
2023-01-16 15:00:48 +0100

    💬 update error default value in textarea example

--- examples/next-typescript/pages/textarea.tsx
+++ examples/next-typescript/pages/textarea.tsx
@@ -27,7 +27,12 @@ const TextareaPage: NextPage = () => {
 
         <p>error</p>
         <Container gap={10}>
-          <Textarea errored defaultValue={"haha"}></Textarea>
+          <Textarea
+            errored
+            defaultValue={
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+            }
+          ></Textarea>
         </Container>
       </Container>
     </>
