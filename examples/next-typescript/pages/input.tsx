1fae0302e2a5fce9b47bd0011eca020309a61ad4
Antoine Kingue <etorefr@gmail.com>
2023-01-19 02:36:01 +0100

    🎨 improve input with prefix, suffix, disable and multiple style options

--- examples/next-typescript/pages/input.tsx
+++ examples/next-typescript/pages/input.tsx
@@ -1,6 +1,7 @@
 import { Container, Text, Input } from "@tonightpass/kitchen";
 import { NextPage } from "next";
 import React from "react";
+import { RiArrowUpCircleLine } from "react-icons/ri";
 
 const ProgressPage: NextPage = () => {
   return (
@@ -19,7 +20,7 @@ const ProgressPage: NextPage = () => {
 
         <Container gap={5}>
           <Text>sizes</Text>
-          <Container direction={["column", "row", "row"]} row>
+          <Container row>
             <Container align="flex-start">
               <Input size="small" placeholder="Small" />
             </Container>
@@ -31,6 +32,139 @@ const ProgressPage: NextPage = () => {
             <Container align="flex-start">
               <Input size="large" placeholder="Large" />
             </Container>
+          </Container>
+        </Container>
+
+        <Container gap={5}>
+          <Text>sizes, prefix, and suffix</Text>
+          <Container gap={5} row>
+            <Container align="flex-start">
+              <Input
+                size="small"
+                placeholder="Small"
+                prefix={<RiArrowUpCircleLine />}
+                suffix={<RiArrowUpCircleLine />}
+              />
+            </Container>
+
+            <Container align="flex-start">
+              <Input
+                placeholder="Default"
+                prefix={<RiArrowUpCircleLine />}
+                suffix={<RiArrowUpCircleLine />}
+              />
+            </Container>
+
+            <Container align="flex-start">
+              <Input
+                size="large"
+                placeholder="Large"
+                prefix={<RiArrowUpCircleLine />}
+                suffix={<RiArrowUpCircleLine />}
+              />
+            </Container>
+          </Container>
+        </Container>
+
+        <Container gap={5}>
+          <Text>disabled</Text>
+          <Container row gap={5}>
+            <Container>
+              <Input placeholder="Disabled with placeholder" disabled />
+            </Container>
+
+            <Container>
+              <Input value="Disabled with value" disabled />
+            </Container>
+          </Container>
+        </Container>
+
+        <Container gap={5} align="flex-start">
+          <Text>prefix and suffix</Text>
+          <Container>
+            <Input placeholder="Default" prefix={<RiArrowUpCircleLine />} />
+          </Container>
+
+          <Container>
+            <Input placeholder="Default" suffix={<RiArrowUpCircleLine />} />
+          </Container>
+
+          <Container>
+            <Input placeholder="Default" prefix="https://" suffix=".com" />
+          </Container>
+
+          <Container>
+            <Input
+              placeholder="Default"
+              prefix={<RiArrowUpCircleLine />}
+              suffix={<RiArrowUpCircleLine />}
+              prefixStyling={false}
+              suffixStyling={false}
+            />
+          </Container>
+
+          <Container>
+            <Input
+              placeholder="Default"
+              prefix="kitchen/"
+              suffix={<RiArrowUpCircleLine />}
+              suffixStyling={false}
+              suffixContainer={false}
+            />
+          </Container>
+        </Container>
+
+        <Container gap={5} align="flex-start">
+          <Text>prefix and suffix</Text>
+          <Container>
+            <Input
+              placeholder="Default"
+              prefix={<RiArrowUpCircleLine />}
+              disabled
+            />
+          </Container>
+
+          <Container>
+            <Input
+              placeholder="Default"
+              suffix={<RiArrowUpCircleLine />}
+              disabled
+            />
+          </Container>
+
+          <Container>
+            <Input
+              placeholder="Default"
+              prefix="https://"
+              suffix=".com"
+              disabled
+            />
+          </Container>
+
+          <Container>
+            <Input
+              placeholder="Default"
+              prefix={<RiArrowUpCircleLine />}
+              suffix={<RiArrowUpCircleLine />}
+              prefixStyling={false}
+              suffixStyling={false}
+              disabled
+            />
+          </Container>
+
+          <Container>
+            <Input
+              placeholder="Default"
+              prefix="vercel/"
+              suffix={
+                <span onClick={() => alert("hey")}>
+                  <RiArrowUpCircleLine />
+                </span>
+              }
+              suffixStyling={false}
+              suffixContainer={false}
+              disabled
+            />
           </Container>
         </Container>
       </Container>
--- packages/kitchen/src/components/Input/index.tsx
+++ packages/kitchen/src/components/Input/index.tsx
@@ -5,27 +5,68 @@ import { KitchenComponent, NormalSizes } from "../../types";
 export type InputProps = KitchenComponent & {
   placeholder?: string;
   size?: NormalSizes;
+  prefix?: React.ReactNode;
+  suffix?: React.ReactNode;
+  value?: string;
+  disabled?: boolean;
+  prefixStyling?: boolean;
+  suffixStyling?: boolean;
+  prefixContainer?: boolean;
+  suffixContainer?: boolean;
 };
 
-const Input = styled(({ size = "normal", ...props }: InputProps) => (
-  <Container>
-    <Field size={size} {...props} />
-  </Container>
-))`
+const Input = styled(
+  ({
+    size = "normal",
+    prefix,
+    suffix,
+    disabled,
+    prefixContainer = true,
+    suffixContainer = true,
+    prefixStyling = true,
+    suffixStyling = true,
+    ...props
+  }: InputProps) => (
+    <Container disabled={disabled}>
+      {prefix && prefixContainer && (
+        <Prefix size={size} disabled={disabled} prefixStyling={prefixStyling}>
+          {prefix}
+        </Prefix>
+      )}
+      <Field
+        size={size}
+        prefix={prefix}
+        suffix={suffix}
+        prefixContainer={prefixContainer}
+        suffixContainer={suffixContainer}
+        prefixStyling={prefixStyling}
+        suffixStyling={suffixStyling}
+        disabled={disabled}
+        {...props}
+      />
+      {suffix && suffixContainer && (
+        <Suffix size={size} disabled={disabled} suffixStyling={suffixStyling}>
+          {suffix}
+        </Suffix>
+      )}
+    </Container>
+  )
+)`
   font: inherit;
   width: 100%;
   min-width: 0;
   display: inline-flex;
   appearance: none;
   line-height: normal;
-  order: 1;
   outline: none;
   transition: border-color 0.2s ease-in-out;
   -webkit-tap-highlight-color: transparent;
   border: 1px solid ${({ theme }) => theme.colors.layout.dark};
   border-radius: ${({ theme }) => theme.radius.square};
-  background-color: ${({ theme }) => theme.colors.layout.darkest};
   color: ${({ theme }) => theme.colors.text.lightest};
+  background-color: ${({ theme, disabled }) =>
+    disabled ? theme.colors.layout.darker : theme.colors.layout.darkest};
+  ${({ disabled }) => disabled && "cursor: not-allowed;"}
 
   ::placeholder {
     color: ${({ theme }) => theme.colors.text.light};
@@ -33,15 +74,122 @@ const Input = styled(({ size = "normal", ...props }: InputProps) => (
   }
 `;
 
-const Container = styled.div`
+const Container = styled.div<{
+  disabled?: InputProps["disabled"];
+}>`
   display: flex;
   align-items: center;
   max-width: 100%;
   font-size: ${({ theme }) => theme.size.small};
+  border-radius: ${({ theme }) => theme.radius.square};
+  ${({ disabled }) => disabled && "cursor: not-allowed;"}
 `;
 
 const Field = styled.input<InputProps>`
   padding: 0 ${({ theme }) => theme.gap.small};
+  height: ${(props) => {
+    switch (props.size) {
+      case "small":
+        return "30px";
+      case "large":
+        return "50px";
+      case "normal":
+      default:
+        return "40px";
+    }
+  }};
+  font-size: ${({ size, theme }) => {
+    switch (size) {
+      case "small":
+        return theme.size.small;
+      case "large":
+        return theme.size.normal;
+      case "normal":
+      default:
+        return "100%";
+    }
+  }};
+
+  ${({ prefix, prefixContainer }) =>
+    prefix &&
+    prefixContainer &&
+    `
+    border-top-left-radius: 0;
+    border-bottom-left-radius: 0;
+  `}
+  ${({ suffix, suffixContainer }) =>
+    suffix &&
+    suffixContainer &&
+    `
+    border-top-right-radius: 0;
+    border-bottom-right-radius: 0;
+  `}
+  ${({ prefix, prefixContainer, prefixStyling }) =>
+    prefix && prefixContainer && !prefixStyling && "border-left: none;"}
+  ${({ suffix, suffixContainer, suffixStyling }) =>
+    suffix && suffixContainer && !suffixStyling && "border-right: none;"}
+`;
+
+const Prefix = styled.span<{
+  size?: InputProps["size"];
+  disabled?: InputProps["disabled"];
+  prefixStyling?: InputProps["prefixStyling"];
+}>`
+  flex-shrink: 0;
+  display: flex;
+  align-items: center;
+  flex-shrink: 0;
+  color: ${({ theme }) => theme.colors.text.light};
+  border: 1px solid ${({ theme }) => theme.colors.layout.dark};
+  border-right: none;
+  border-radius: ${({ theme }) => theme.radius.square} 0 0
+    ${({ theme }) => theme.radius.square};
+  padding: 0 ${({ theme }) => theme.gap.small};
+  ${({ theme, prefixStyling, disabled }) =>
+    (prefixStyling || disabled) &&
+    `background-color: ${theme.colors.layout.darker};`};
+  height: ${(props) => {
+    switch (props.size) {
+      case "small":
+        return "30px";
+      case "large":
+        return "50px";
+      case "normal":
+      default:
+        return "40px";
+    }
+  }};
+  font-size: ${({ size, theme }) => {
+    switch (size) {
+      case "small":
+        return theme.size.small;
+      case "large":
+        return theme.size.normal;
+      case "normal":
+      default:
+        return "100%";
+    }
+  }};
+`;
+
+const Suffix = styled.span<{
+  size?: InputProps["size"];
+  disabled?: InputProps["disabled"];
+  suffixStyling?: InputProps["suffixStyling"];
+}>`
+  flex-shrink: 0;
+  display: flex;
+  align-items: center;
+  flex-shrink: 0;
+  color: ${({ theme }) => theme.colors.text.light};
+  border: 1px solid ${({ theme }) => theme.colors.layout.dark};
+  border-left: none;
+  border-radius: 0 ${({ theme }) => theme.radius.square}
+    ${({ theme }) => theme.radius.square} 0;
+  padding: 0 ${({ theme }) => theme.gap.small};
+  ${({ theme, suffixStyling, disabled }) =>
+    (suffixStyling || disabled) &&
+    `background-color: ${theme.colors.layout.darker};`};
   height: ${(props) => {
     switch (props.size) {
       case "small":
