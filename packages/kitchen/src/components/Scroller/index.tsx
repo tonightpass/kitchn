bd10ee464602f5a04105697b318ad8e292b05d78
Younès Bessa <bessa460@gmail.com>
2023-01-23 10:58:06 +0100

    🚨 fix codacy error

--- packages/kitchen/src/components/Scroller/index.tsx
+++ packages/kitchen/src/components/Scroller/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { KitchenComponent, NormalSizes } from "../../types";
+import { KitchenComponent } from "../../types";
 
 export type ScrollerProps = KitchenComponent & {
   /**
