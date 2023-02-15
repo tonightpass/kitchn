106fa16917a6bee0ed88c4c9f6075c2ae8550282
Antoine Kingue <etorefr@gmail.com>
2023-02-15 15:49:35 +0100

    ✏️ update landing features content

--- docs/components/Landing/Features/index.tsx
+++ docs/components/Landing/Features/index.tsx
@@ -21,33 +21,101 @@ const Features = () => {
       <Container mt={"large"} w={"100%"} gap={"normal"}>
         <Container direction={["column", "column", "row"]} gap={"normal"}>
           <Container gap={"normal"}>
-            <BlueContainer minH={200}>Accessible</BlueContainer>
-            <BlueContainer minH={100}>Responsive support</BlueContainer>
+            <FeatureContainer minH={200} p={"medium"}>
+              <Text size={"large"} weight={"bold"}>
+                Accessible
+              </Text>
+              <Text mt={"small"} color={"lighter"}>
+                Kitchen strictly follows WAI-ARIA standards for all components.
+              </Text>
+            </FeatureContainer>
+            <FeatureContainer minH={100} p={"medium"}>
+              <Text size={"large"} weight={"bold"}>
+                Responsive support
+              </Text>
+              <Text mt={"small"} color={"lighter"}>
+                Build simple web apps with our simple responsive system.
+              </Text>
+            </FeatureContainer>
           </Container>
           <Container gap={"normal"}>
-            <BlueContainer minH={100}>Themeable</BlueContainer>
-            <BlueContainer minH={200}>All platform support</BlueContainer>
+            <FeatureContainer minH={100} p={"medium"}>
+              <Text size={"large"} weight={"bold"}>
+                Themeable
+              </Text>
+              <Text mt={"small"} color={"lighter"}>
+                Customize any part of our components to match your design needs.
+              </Text>
+            </FeatureContainer>
+            <FeatureContainer minH={200} p={"medium"}>
+              <Text size={"large"} weight={"bold"}>
+                All platform support
+              </Text>
+              <Text mt={"small"} color={"lighter"}>
+                Whether you use React, React Native or a framework react,
+                Kitchen is compatible with all platforms.
+              </Text>
+            </FeatureContainer>
           </Container>
           <Container gap={"normal"}>
-            <BlueContainer minH={200}>Active Community</BlueContainer>
-            <BlueContainer minH={100}>Reusable hooks</BlueContainer>
+            <FeatureContainer minH={200} p={"medium"}>
+              <Text size={"large"} weight={"bold"}>
+                Active Community
+              </Text>
+              <Text mt={"small"} color={"lighter"}>
+                We are a team of active maintainers ready to help you whenever
+                you need, and using it for our own products.
+              </Text>
+            </FeatureContainer>
+            <FeatureContainer minH={100} p={"medium"}>
+              <Text size={"large"} weight={"bold"}>
+                Reusable hooks
+              </Text>
+              <Text mt={"small"} color={"lighter"}>
+                Lots of useful hooks to use in your applications.
+              </Text>
+            </FeatureContainer>
           </Container>
         </Container>
-        <BlueContainer>Developer experience</BlueContainer>
+        <FeatureContainer p={"medium"}>
+          <Text size={"large"} weight={"bold"}>
+            Developer experience
+          </Text>
+          <Text mt={"small"} color={"lighter"}>
+            Guaranteed to boost your productivity when building your mobile app
+            or website.
+          </Text>
+        </FeatureContainer>
         <Container direction={["column", "row"]} gap={"normal"}>
-          <BlueContainer>Light & Dark support</BlueContainer>
-          <BlueContainer>Stylable</BlueContainer>
+          <FeatureContainer p={"medium"}>
+            <Text size={"large"} weight={"bold"}>
+              Light & Dark support
+            </Text>
+            <Text mt={"small"} color={"lighter"}>
+              Kitchen strictly follows WAI-ARIA standards for all components.
+            </Text>
+          </FeatureContainer>
+          <FeatureContainer p={"medium"}>
+            <Text size={"large"} weight={"bold"}>
+              Stylable
+            </Text>
+            <Text mt={"small"} color={"lighter"}>
+              Easy to redesign with your standard thanks to styled components
+              already implemented.
+            </Text>
+          </FeatureContainer>
         </Container>
       </Container>
     </Container>
   );
 };
 
-const BlueContainer = kitchen(Container)`
-  background: ${({ theme }) => theme.colors.accent.info};
-  color: ${({ theme }) => theme.colors.accent.light};
-  padding: 10px;
-  border: 1px solid ${({ theme }) => theme.colors.accent.light};
+const FeatureContainer = kitchen(Container)`
+  background-color: ${({ theme }) => theme.colors.layout.darker};
+
+  @media (min-width: ${({ theme }) => theme.breakpoint.mobile}) {
+    
+  }
 `;
 
 export default Features;
