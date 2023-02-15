951a85cc9a0a993d6c810622dbb9bb29742f53dd
Antoine Kingue <etorefr@gmail.com>
2023-02-15 17:55:15 +0100

    💄 fix sandpack font on landing

--- docs/components/Landing/Render/index.tsx
+++ docs/components/Landing/Render/index.tsx
@@ -1,5 +1,5 @@
 import { Sandpack } from "@codesandbox/sandpack-react";
-import { Container, useTheme } from "@tonightpass/kitchen";
+import kitchen, { Container, useTheme } from "@tonightpass/kitchen";
 
 const Render: React.FC = () => {
   const { theme } = useTheme();
@@ -11,7 +11,7 @@ import "@tonightpass/kitchen/fonts.css";
 export default function App() {
   return (
     <KitchenProvider>
-      <Container h={"100%"} align={"center"}>
+      <Container h={"100vh"} align={"center"} justify={"center"}>
         <Button>Button</Button>
       </Container>
     </KitchenProvider>
@@ -19,22 +19,31 @@ export default function App() {
 }`;
 
   return (
-    <Container id={"render"} section>
+    <StyledContainer id={"render"} section>
       <Sandpack
-        template="react-ts"
+        template={"react-ts"}
         theme={theme.id === 2 ? "light" : "dark"}
         files={{
           "/App.tsx": code,
         }}
         customSetup={{
           dependencies: {
-            "@tonightpass/kitchen": "latest",
+            "@tonightpass/kitchen": "1.0.0",
             next: "latest", // TODO: remove this after @tonightpass/kitchen is published
           },
         }}
       />
-    </Container>
+    </StyledContainer>
   );
 };
+
+const StyledContainer = kitchen(Container)`
+  font-family: ${({ theme }) => theme.family.monospace};
+  font-size: 14px;
+  body, div, span, input, button, textarea, select, option {
+    font-family: inherit;
+    font-size: inherit;
+  }
+`;
 
 export default Render;
