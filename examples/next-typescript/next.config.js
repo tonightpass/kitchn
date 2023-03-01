2777990add7f99ab8413b43c9cffbb3b58038d80
Antoine Kingue <etorefr@gmail.com>
2023-03-01 15:19:46 +0100

    🔥 remove preconstruct

--- examples/next-typescript/next.config.js
+++ examples/next-typescript/next.config.js
@@ -1,5 +1,4 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const withPreconstruct = require("@preconstruct/next");
 const { withKitchen } = require("@tonightpass/kitchen/next");
 
-module.exports = withPreconstruct(withKitchen());
+module.exports = withKitchen();
