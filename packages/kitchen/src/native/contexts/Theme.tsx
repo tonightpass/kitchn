4e096e94abc70bd50a789c95b4bf61c1abb51032
Antoine Kingue <etorefr@gmail.com>
2023-03-08 13:17:31 +0100

    🎨 add `native` theme detection

---
+++ packages/kitchen/src/native/contexts/Theme.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { useColorScheme } from "react-native";
+import {
+  DefaultTheme,
+  ThemeProvider as StyledThemeProvider,
+} from "styled-components/native";
+import themes from "../themes";
+
+const ThemeContext = React.createContext({
+  theme: themes.dark,
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  setTheme: (_theme: DefaultTheme) => {},
+});
+
+type ThemeProviderProps = {
+  children: React.ReactNode;
+  theme?: DefaultTheme;
+};
+
+/**
+ * System: 0
+ * Dark: 1
+ * Light: 2
+ */
+
+const ThemeProvider = ({
+  children,
+  theme: customTheme,
+  ...props
+}: ThemeProviderProps) => {
+  const colorScheme = useColorScheme();
+
+  const isDarkTheme = colorScheme === "dark";
+
+  const [theme, setTheme] = React.useState<DefaultTheme>(
+    customTheme || (isDarkTheme ? themes.dark : themes.light)
+  );
+
+  React.useEffect(() => {
+    if (customTheme) {
+      setTheme(customTheme);
+    } else if (!isDarkTheme) {
+      setTheme(themes.light);
+    } else {
+      setTheme(themes.dark);
+    }
+  }, [customTheme, isDarkTheme]);
+
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }} {...props}>
+      <StyledThemeProvider theme={theme}>{children}</StyledThemeProvider>
+    </ThemeContext.Provider>
+  );
+};
+
+export { ThemeContext, ThemeProvider };
--- packages/kitchen/src/native/index.ts
+++ packages/kitchen/src/native/index.ts
@@ -18,6 +18,9 @@ export type { KitchenProviderProps } from "./components/Provider";
 export { default as Text } from "./components/Text";
 export type { TextProps } from "./components/Text";
 
+// ./hooks
+export { default as useTheme } from "./hooks/useTheme";
+
 // .hoc
 export { default as withScale } from "./hoc/withScale";
 export type { ScaleProps } from "./hoc/withScale";
