0a274f64b06f2b7c255e1be3401d32788626ec4d
Antoine Kingue <etorefr@gmail.com>
2023-03-09 19:28:37 +0100

    ✨ add native container

--- packages/kitchen/src/native/index.ts
+++ packages/kitchen/src/native/index.ts
@@ -13,9 +13,14 @@ export { default as themes, createTheme } from "./themes";
 export { default as KitchenProvider } from "./components/Provider";
 export type { KitchenProviderProps } from "./components/Provider";
 
-export { default as Text, type TextProps } from "./components/Text";
-
 export { default as Button, type ButtonProps } from "./components/Button";
+
+export {
+  default as Container,
+  type ContainerProps,
+} from "./components/Container";
+
+export { default as Text, type TextProps } from "./components/Text";
 
 // ./hooks
 export { default as useTheme } from "./hooks/useTheme";
