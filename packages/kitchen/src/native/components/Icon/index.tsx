b0ce97ac5ff59cd5b95b1e56598d4593dfa47252
Antoine Kingue <etorefr@gmail.com>
2023-03-20 13:20:45 +0100

    ✨ add native input and icon

---
+++ packages/kitchen/src/native/components/Icon/index.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import RemixIcon from "react-native-remix-icon";
+import useTheme from "../../hooks/useTheme";
+import { KitchenComponent, Size, TextColors } from "../../types";
+
+type Props = {
+  name: string;
+  size?: number | string | keyof Size;
+  fill?: boolean;
+  color?: keyof TextColors | string;
+};
+
+export type IconProps = KitchenComponent<Props>;
+
+const Icon = ({ name, size = "normal", fill, color, ...props }: IconProps) => {
+  const { theme } = useTheme();
+  return (
+    <RemixIcon
+      name={`${name}-${fill ? "fill" : "line"}`}
+      size={theme.size[size as keyof Size] || size || theme.size.normal}
+      color={
+        theme.colors.text[color as keyof TextColors] ||
+        color ||
+        theme.colors.text.lightest
+      }
+      {...props}
+    />
+  );
+};
+
+export default Icon;
