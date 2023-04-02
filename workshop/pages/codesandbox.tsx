96df5af4ad024d29583fe5e5b2d406716064c774
Antoine Kingue <etorefr@gmail.com>
2023-04-03 00:26:17 +0200

    💄 update sandbox example

--- workshop/pages/codesandbox.tsx
+++ workshop/pages/codesandbox.tsx
@@ -3,55 +3,38 @@ import { Button, Container, Image, Text } from "@tonightpass/kitchen";
 const CodeSandboxPage = () => {
   return (
     <Container h={"100vh"} align={"center"} justify={"center"}>
-      <Container
-        w={600}
-        h={600}
-        flex={"initial"}
-        align={"center"}
-        justify={"center"}
-      >
-        {/* Example start */}
-        <Container maxW={245}>
-          <Image
-            src={
-              "https://unsplash.com/photos/RUvW1KGD9a4/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MTB8fGNoYW1icmV8ZnJ8MHx8fHwxNjgwNDcwNjM2&w=640"
-            }
-            br={"square"}
-            alt={"Example"}
-            w={"100%"}
-          />
-          <Container
-            mt={"small"}
-            align={"center"}
-            justify={"space-between"}
-            row
-          >
-            <Text weight={"bold"}>Apartment in Paris</Text>
-            <Text>
-              ⭐ <Text weight={"bold"}>4.9</Text> (20)
-            </Text>
-          </Container>
-          <Container mt={"tiny"}>
-            <Text color={"lighter"}>Large 2 rooms flat with a terrace</Text>
-            <Text color={"lighter"}>1 king bed</Text>
-            <Text color={"lighter"}>Aug 19 - 26</Text>
-          </Container>
-          <Text color={"lighter"} mt={"tiny"}>
-            <Text color={"lighter"} decoration={"line-through"}>
-              $114
-            </Text>{" "}
-            <Text color={"lightest"} weight={"bold"}>
-              $120
-            </Text>{" "}
-            <Text color={"lightest"}>{"night ·"}</Text> $735 total
+      <Container maxW={245}>
+        <Image
+          src={
+            "https://unsplash.com/photos/RUvW1KGD9a4/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MTB8fGNoYW1icmV8ZnJ8MHx8fHwxNjgwNDcwNjM2&w=640"
+          }
+          br={"square"}
+          alt={"Example"}
+          w={"100%"}
+        />
+        <Container mt={"small"} align={"center"} justify={"space-between"} row>
+          <Text weight={"bold"}>Apartment in Paris</Text>
+          <Text>
+            ⭐ <Text weight={"bold"}>4.9</Text> (20)
           </Text>
-          <Container>
-            <Button mt={"small"} w={"100%"}>
-              Book now
-            </Button>
-          </Container>
         </Container>
-        {/* Example end */}
+        <Container mt={"tiny"}>
+          <Text color={"lighter"}>Large 2 rooms flat with a terrace</Text>
+          <Text color={"lighter"}>1 king bed</Text>
+          <Text color={"lighter"}>Aug 19 - 26</Text>
+        </Container>
+        <Text color={"lighter"} mt={"tiny"}>
+          <Text color={"lighter"} decoration={"line-through"}>
+            $114
+          </Text>{" "}
+          <Text color={"lightest"} weight={"bold"}>
+            $120
+          </Text>{" "}
+          <Text color={"lightest"}>{"night ·"}</Text> $735 total
+        </Text>
+        <Container>
+          <Button mt={"small"}>Book now</Button>
+        </Container>
       </Container>
     </Container>
   );
