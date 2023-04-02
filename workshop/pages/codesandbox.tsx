9ca6b7ac9d8323a628447f4e6a2c8e52e6f2385b
Antoine Kingue <etorefr@gmail.com>
2023-04-02 23:58:37 +0200

    🎨 improve docs with new example

--- workshop/pages/codesandbox.tsx
+++ workshop/pages/codesandbox.tsx
@@ -1,18 +1,16 @@
-import { Sandpack } from "@codesandbox/sandpack-react";
-import { nightOwl } from "@codesandbox/sandpack-themes";
-import kitchen, { Container, useTheme } from "@tonightpass/kitchen";
+import { Button, Container, Image, Text } from "@tonightpass/kitchen";
 
-const Render: React.FC = () => {
-  const { theme } = useTheme();
-
-  const code = `import { KitchenProvider, Button, Container, Image, Text } from "@tonightpass/kitchen";
-
-import "@tonightpass/kitchen/fonts.css";
-
-export default function App() {
+const CodeSandboxPage = () => {
   return (
-    <KitchenProvider>
-      <Container h={"100vh"} align={"center"} justify={"center"}>
+    <Container h={"100vh"} align={"center"} justify={"center"}>
+      <Container
+        w={600}
+        h={600}
+        flex={"initial"}
+        align={"center"}
+        justify={"center"}
+      >
+        {/* Example start */}
         <Container maxW={245}>
           <Image
             src={
@@ -53,41 +51,10 @@ export default function App() {
             </Button>
           </Container>
         </Container>
+        {/* Example end */}
       </Container>
-    </KitchenProvider>
-  );
-}`;
-
-  return (
-    <StyledContainer id={"render"} section>
-      <Sandpack
-        template={"react-ts"}
-        theme={theme.id === 2 ? "light" : nightOwl}
-        options={{
-          editorHeight: 600,
-        }}
-        files={{
-          "/App.tsx": code,
-        }}
-        customSetup={{
-          dependencies: {
-            "@tonightpass/kitchen": "latest",
-          },
-        }}
-      />
-    </StyledContainer>
+    </Container>
   );
 };
 
-const StyledContainer = kitchen(Container)`
-  border-radius: ${({ theme }) => theme.radius.square};
-  overflow: hidden;
-  font-family: ${({ theme }) => theme.family.monospace};
-  font-size: 14px;
-  body, div, span, input, button, textarea, select, option {
-    font-family: inherit;
-    font-size: inherit;
-  }
-`;
-
-export default Render;
+export default CodeSandboxPage;
