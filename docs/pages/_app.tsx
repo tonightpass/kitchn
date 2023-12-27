80ffec726af701712d702a3669ea7b7f18c5b345
Antoine Kingue <contact@antoinek.fr>
2023-12-27 14:42:57 +0100

    🩹 fix kitchen docs with a provider

--- docs/pages/_app.tsx
+++ docs/pages/_app.tsx
@@ -1,14 +1,15 @@
 import GlobalStyle from "@components/GlobalStyle";
+import { KitchenProvider } from "@tonightpass/kitchen";
 import { AppProps } from "next/app";
 
 import "@tonightpass/kitchen/fonts.css";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <>
+    <KitchenProvider>
       <GlobalStyle />
       <Component {...pageProps} />
-    </>
+    </KitchenProvider>
   );
 };
 
