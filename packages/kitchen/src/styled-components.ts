99e2f50985fb440c561c74fa7ec7da6ba3f28dc3
Antoine Kingue <contact@antoinek.fr>
2024-01-26 17:22:03 +0100

    🩹 fix styled-components exports

--- packages/kitchen/src/styled-components.ts
+++ packages/kitchen/src/styled-components.ts
@@ -2,19 +2,25 @@ import "styled-components";
 
 import { MainTheme, Theme } from "./types/theme";
 
-// custom exports
-
 // custom types
-
 declare module "styled-components" {
   export interface DefaultTheme extends Theme, MainTheme {}
 }
 
+// custom exports
 export {
   ThemeContext as StyledThemeContext,
   ThemeProvider as StyledThemeProvider,
   useTheme as useStyledTheme,
+  ServerStyleSheet,
+  StyleSheetConsumer,
+  StyleSheetContext,
+  StyleSheetManager,
+  ThemeConsumer,
+  createGlobalStyle,
+  css,
+  isStyledComponent,
+  keyframes,
+  withTheme,
   default as kitchen,
 } from "styled-components";
-
-export * from "styled-components";
