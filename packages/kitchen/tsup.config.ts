a55efe1f5647ca6c144c64069910feb3df89132c
Antoine Kingue <contact@antoinek.fr>
2024-01-29 13:20:03 +0100

    🔧 update tsup and ts config

--- packages/kitchen/tsup.config.ts
+++ packages/kitchen/tsup.config.ts
@@ -1,18 +1,25 @@
 import { defineConfig, Options } from "tsup";
 
 const config: Options = {
+  format: ["cjs", "esm"],
   entry: [
     "src/index.ts",
     "src/next/index.ts",
     // "src/native/index.ts"
   ],
-  splitting: false,
+  splitting: true,
   sourcemap: true,
   clean: true,
-  platform: "node",
+  platform: "browser",
   dts: true,
   minify: true,
   treeshake: true,
+  outExtension({ format }) {
+    return {
+      js: `.${format}.js`,
+      mjs: `.${format}.js`,
+    };
+  },
 };
 
 export default defineConfig(config);
