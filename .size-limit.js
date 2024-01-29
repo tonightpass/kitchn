d9d27e19f89147b52095f068c387bea2d5a29ad2
Antoine Kingue <contact@antoinek.fr>
2024-01-29 21:59:46 +0100

    🔧 update size limit config

--- .size-limit.js
+++ .size-limit.js
@@ -9,25 +9,32 @@ const modifyWebpackConfig = (config) => {
     },
     externals: {
       "react-native": true,
+      "react-native-remix-icon": true,
     },
   };
 };
 
 module.exports = [
   {
-    path: "packages/kitchen/dist/tonightpass-kitchen.cjs.prod.js",
+    path: "packages/kitchen/dist/index.cjs.js",
+  },
+  {
+    path: "packages/kitchen/dist/index.esm.js",
+  },
+  {
+    path: "packages/kitchen/dist/next/index.cjs.js",
     modifyWebpackConfig,
   },
   {
-    path: "packages/kitchen/dist/tonightpass-kitchen.esm.js",
+    path: "packages/kitchen/dist/next/index.esm.js",
     modifyWebpackConfig,
   },
   {
-    path: "packages/kitchen/next/dist/tonightpass-kitchen-next.cjs.prod.js",
+    path: "packages/kitchen/dist/native/index.cjs.js",
     modifyWebpackConfig,
   },
   {
-    path: "packages/kitchen/next/dist/tonightpass-kitchen-next.esm.js",
+    path: "packages/kitchen/dist/native/index.esm.js",
     modifyWebpackConfig,
   },
 ];
