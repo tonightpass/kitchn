301542da3a82872bf81947c593fbf49e85772934
Antoine Kingue <contact@antoinek.fr>
2024-02-16 00:48:37 +0100

    ➖ remove @codesanbox/sandpack

--- docs/pages/_document.tsx
+++ docs/pages/_document.tsx
@@ -1,4 +1,3 @@
-import { getSandpackCssText } from "@codesandbox/sandpack-react";
 import { KitchenDocument } from "@tonightpass/kitchen/next";
 import { Head, Html, Main, NextScript } from "next/document";
 
@@ -6,13 +5,7 @@ export default class MyDocument extends KitchenDocument {
   render() {
     return (
       <Html>
-        <Head>
-          <style
-            dangerouslySetInnerHTML={{ __html: getSandpackCssText() }}
-            id={"sandpack"}
-            key={"sandpack-css"}
-          />
-        </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
