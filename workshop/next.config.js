65cafeabcb4adb19e3e55562b31c2fc7bcd2bdcd
Antoine Kingue <contact@antoinek.fr>
2024-02-26 12:45:53 +0100

    ⚰️ remove preconstruct

--- workshop/next.config.js
+++ workshop/next.config.js
@@ -1,5 +1,4 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const withPreconstruct = require("@preconstruct/next");
 const { withKitchenConfig } = require("@tonightpass/kitchen/next");
 
-module.exports = withPreconstruct(withKitchenConfig());
+module.exports = withKitchenConfig();
