c7c95dcda1434a23e9f051f1a60d8c1a7fb550ed
Antoine Kingue <contact@antoinek.fr>
2024-03-07 15:38:09 +0100

    🚨 fix lint errors

--- workshop/pages/fieldset.tsx
+++ workshop/pages/fieldset.tsx
@@ -25,25 +25,25 @@ const FieldsetPage: NextPage = () => {
           margin: "0 auto",
         }}
       >
-        <Text>default</Text>
+        <Text>{"default"}</Text>
         <Container gap={10}>
           <Fieldset>
             <Content>
-              <Title>The Evil Rabbit Jumped over the Fence</Title>
-              <Subtitle>The Evil Rabbit Jumped over the Fence</Subtitle>
+              <Title>{"The Evil Rabbit Jumped over the Fence"}</Title>
+              <Subtitle>{"The Evil Rabbit Jumped over the Fence"}</Subtitle>
             </Content>
             <Footer>
               <Status accent={"light"}>
-                The Evil Rabbit Jumped over the Fence
+                {"The Evil Rabbit Jumped over the Fence"}
               </Status>
               <Action>
-                <Button size={"small"}>Action</Button>
+                <Button size={"small"}>{"Action"}</Button>
               </Action>
             </Footer>
           </Fieldset>
         </Container>
 
-        <Text>tabs</Text>
+        <Text>{"tabs"}</Text>
         <Container gap={10}>
           <Tabs
             tabs={[
@@ -56,154 +56,154 @@ const FieldsetPage: NextPage = () => {
           {selected === "one" ? (
             <Fieldset>
               <Content>
-                <Title>The Evil Rabbit Jumped over the Fence</Title>
-                <Subtitle>The Evil Rabbit Jumped over the Fence</Subtitle>
+                <Title>{"The Evil Rabbit Jumped over the Fence"}</Title>
+                <Subtitle>{"The Evil Rabbit Jumped over the Fence"}</Subtitle>
               </Content>
               <Footer>
                 <Status accent={"light"}>
-                  The Evil Rabbit Jumped over the Fence
+                  {"The Evil Rabbit Jumped over the Fence"}
                 </Status>
                 <Action>
-                  <Button size={"small"}>Action</Button>
+                  <Button size={"small"}>{"Action"}</Button>
                 </Action>
               </Footer>
             </Fieldset>
           ) : (
             <Fieldset>
               <Content>
-                <Title>The Fence Jumped over The Evil Rabbit</Title>
-                <Subtitle>The Fence Jumped over The Evil Rabbit</Subtitle>
+                <Title>{"The Fence Jumped over The Evil Rabbit"}</Title>
+                <Subtitle>{"The Fence Jumped over The Evil Rabbit"}</Subtitle>
               </Content>
               <Footer>
                 <Status accent={"light"}>
-                  The Fence Jumped over The Evil Rabbit
+                  {"The Fence Jumped over The Evil Rabbit"}
                 </Status>
                 <Action>
-                  <Button size={"small"}>Action</Button>
+                  <Button size={"small"}>{"Action"}</Button>
                 </Action>
               </Footer>
             </Fieldset>
           )}
         </Container>
 
-        <Text>error</Text>
+        <Text>{"error"}</Text>
         <Container gap={10}>
           <Fieldset>
             <Content>
-              <Title>The Evil Rabbit Jumped over the Fence</Title>
-              <Subtitle>The Evil Rabbit Jumped over the Fence</Subtitle>
+              <Title>{"The Evil Rabbit Jumped over the Fence"}</Title>
+              <Subtitle>{"The Evil Rabbit Jumped over the Fence"}</Subtitle>
             </Content>
             <Footer>
-              <Status accent={"danger"}>An error has occurred.</Status>
+              <Status accent={"danger"}>{"An error has occurred."}</Status>
               <Action>
                 <Button size={"small"} disabled>
-                  Action
+                  {"Action"}
                 </Button>
               </Action>
             </Footer>
           </Fieldset>
         </Container>
 
-        <Text>disabled</Text>
+        <Text>{"disabled"}</Text>
         <Container gap={10}>
           <Fieldset disabled>
             <Content>
               <Title color={"darker"}>
-                The Evil Rabbit Jumped over the Fence
+                {"The Evil Rabbit Jumped over the Fence"}
               </Title>
               <Subtitle color={"darker"}>
-                The Evil Rabbit Jumped over the Fence
+                {"The Evil Rabbit Jumped over the Fence"}
               </Subtitle>
             </Content>
             <Footer disabled>
-              <Status color={"darker"}>An error has occurred.</Status>
+              <Status color={"darker"}>{"An error has occurred."}</Status>
               <Action>
                 <Button size={"small"} disabled>
-                  Action
+                  {"Action"}
                 </Button>
               </Action>
             </Footer>
           </Fieldset>
         </Container>
 
-        <Text>disabled content only</Text>
+        <Text>{"disabled content only"}</Text>
         <Container gap={10}>
           <Fieldset>
             <Content disabled>
               <Title color={"darker"}>
-                The Evil Rabbit Jumped over the Fence
+                {"The Evil Rabbit Jumped over the Fence"}
               </Title>
               <Subtitle color={"darker"}>
-                The Evil Rabbit Jumped over the Fence
+                {"The Evil Rabbit Jumped over the Fence"}
               </Subtitle>
             </Content>
             <Footer>
               <Status accent={"light"}>
-                The Evil Rabbit Jumped over the Fence
+                {"The Evil Rabbit Jumped over the Fence"}
               </Status>
               <Action>
-                <Button size={"small"}>Action</Button>
+                <Button size={"small"}>{"Action"}</Button>
               </Action>
             </Footer>
           </Fieldset>
         </Container>
 
-        <Text>disabled footer only</Text>
+        <Text>{"disabled footer only"}</Text>
         <Container gap={10}>
           <Fieldset>
             <Content>
-              <Title>The Evil Rabbit Jumped over the Fence</Title>
-              <Subtitle>The Evil Rabbit Jumped over the Fence</Subtitle>
+              <Title>{"The Evil Rabbit Jumped over the Fence"}</Title>
+              <Subtitle>{"The Evil Rabbit Jumped over the Fence"}</Subtitle>
             </Content>
             <Footer disabled>
               <Status color={"darker"}>
-                The Evil Rabbit Jumped over the Fence
+                {"The Evil Rabbit Jumped over the Fence"}
               </Status>
               <Action>
                 <Button size={"small"} disabled>
-                  Action
+                  {"Action"}
                 </Button>
               </Action>
             </Footer>
           </Fieldset>
         </Container>
 
-        <Text>highlight footer</Text>
+        <Text>{"highlight footer"}</Text>
         <Container gap={10}>
           <Fieldset>
             <Content disabled>
               <Title color={"darker"}>
-                The Evil Rabbit Jumped over the Fence
+                {"The Evil Rabbit Jumped over the Fence"}
               </Title>
               <Subtitle color={"darker"}>
-                The Evil Rabbit Jumped over the Fence
+                {"The Evil Rabbit Jumped over the Fence"}
               </Subtitle>
             </Content>
             <Footer highlight>
               <Status color={"light"}>
-                The Evil Rabbit Jumped over the Fence
+                {"The Evil Rabbit Jumped over the Fence"}
               </Status>
               <Action>
-                <Button size={"small"}>Action</Button>
+                <Button size={"small"}>{"Action"}</Button>
               </Action>
             </Footer>
           </Fieldset>
         </Container>
 
-        <Text>error</Text>
+        <Text>{"error"}</Text>
         <Container gap={10}>
-          <Fieldset type="error">
+          <Fieldset type={"error"}>
             <Content>
-              <Title>The Evil Rabbit Jumped over the Fence</Title>
-              <Subtitle>The Evil Rabbit Jumped over the Fence</Subtitle>
+              <Title>{"The Evil Rabbit Jumped over the Fence"}</Title>
+              <Subtitle>{"The Evil Rabbit Jumped over the Fence"}</Subtitle>
             </Content>
             <Footer>
               <Status accent={"light"}>
-                The Evil Rabbit Jumped over the Fence
+                {"The Evil Rabbit Jumped over the Fence"}
               </Status>
               <Action>
                 <Button type={"danger"} size={"small"}>
-                  Action
+                  {"Action"}
                 </Button>
               </Action>
             </Footer>
