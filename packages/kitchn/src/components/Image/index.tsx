830610a062847b3b12a5dc2dd1db58b56d84d411
Antoine Kingue <contact@antoinek.fr>
2024-07-16 01:39:30 +0200

    🎨 improve image with sizes attributes

--- packages/kitchn/src/components/Image/index.tsx
+++ packages/kitchn/src/components/Image/index.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import styled from "styled-components";
 
-import { withDecorator } from "../../hoc/withDecorator";
+import { AreaProps, withDecorator } from "../../hoc/withDecorator";
 import { KitchnComponent } from "../../types";
 
 type Props = {
+  width?: AreaProps["width"];
+  w?: Props["width"];
+  height?: AreaProps["height"];
+  h?: Props["height"];
   src: string;
   alt: string;
 };
@@ -14,9 +18,23 @@ export type ImageProps = KitchnComponent<
   React.ImgHTMLAttributes<HTMLImageElement>
 >;
 
-const ImageComponent = styled(({ src, alt, ...props }: ImageProps) => {
-  return <img src={src} alt={alt} draggable={false} {...props} />;
-})<ImageProps>``;
+const ImageComponent = styled(
+  ({ src, alt, width, w, height, h, ...props }: ImageProps) => {
+    const widthValue = width || w || height || h;
+    const heightValue = height || h || width || w;
+
+    return (
+      <img
+        src={src}
+        alt={alt}
+        draggable={false}
+        width={widthValue}
+        height={heightValue}
+        {...props}
+      />
+    );
+  },
+)<ImageProps>``;
 
 ImageComponent.displayName = "KitchnImage";
 export const Image = withDecorator(ImageComponent);
