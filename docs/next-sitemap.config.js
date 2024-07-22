9978ba90ec8ab6a068c54777a872a669d3859741
Antoine Kingue <contact@antoinek.fr>
2024-07-22 16:36:41 +0200

    🔧 update sitemap config

--- docs/next-sitemap.config.js
+++ docs/next-sitemap.config.js
@@ -1,15 +1,12 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const packageJson = require("kitchn/package.json");
-const { env } = require("process");
 
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: packageJson.homepage,
   generateRobotsTxt: true,
   robotsTxtOptions: {
-    policies:
-      env.NODE_ENV === "production"
-        ? [{ userAgent: "*", allow: "/" }]
-        : [{ userAgent: "*", disallow: "/" }],
+    policies: [{ userAgent: "*", allow: "/" }],
+    additionalSitemaps: [`${packageJson.homepage}/sitemap.xml`],
   },
 };
