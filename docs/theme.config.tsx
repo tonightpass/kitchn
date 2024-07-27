589c2f245861e7419ed2ec569fd70043fc8868fc
Antoine Kingue <contact@antoinek.fr>
2024-07-27 19:35:33 +0200

    💄 update primary color

--- docs/theme.config.tsx
+++ docs/theme.config.tsx
@@ -5,6 +5,41 @@ import { Container, Link } from "kitchn";
 import { DocsThemeConfig, useConfig } from "nextra-theme-docs";
 import urlcat from "urlcat";
 
+const rgbToHsl = (r, g, b) => {
+  r /= 255;
+  g /= 255;
+  b /= 255;
+
+  const max = Math.max(r, g, b);
+  const min = Math.min(r, g, b);
+  let h,
+    s,
+    l = (max + min) / 2;
+
+  if (max === min) {
+    h = s = 0; // achromatic
+  } else {
+    const d = max - min;
+    s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+    switch (max) {
+      case r:
+        h = (g - b) / d + (g < b ? 6 : 0);
+        break;
+      case g:
+        h = (b - r) / d + 2;
+        break;
+      case b:
+        h = (r - g) / d + 4;
+        break;
+    }
+    h /= 6;
+  }
+
+  return [h * 360, s * 100, l * 100];
+};
+
+const [h, s] = rgbToHsl(80, 60, 245);
+
 const config: DocsThemeConfig = {
   project: {
     link: "https://github.com/tonightpass/kitchn",
@@ -99,8 +134,8 @@ const config: DocsThemeConfig = {
   editLink: {
     text: "Edit this page on GitHub",
   },
-  primaryHue: 246.49,
-  primarySaturation: 75.51,
+  primaryHue: h,
+  primarySaturation: s,
   footer: {
     component: Footer,
   },
