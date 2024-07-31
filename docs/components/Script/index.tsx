27cc662dc2e2df8aabbba7a92a712e8e9d669c6e
Antoine Kingue <contact@antoinek.fr>
2024-07-31 17:06:24 +0200

    📈 update umami website id

--- docs/components/Script/index.tsx
+++ docs/components/Script/index.tsx
@@ -5,7 +5,7 @@ const Script: React.FC = () => (
     <NextScript
       defer
       src={"https://eu.umami.is/script.js"}
-      data-website-id={"5be27e80-d091-4dfb-982f-86086f9f716e"}
+      data-website-id={"e0f4f54b-03aa-4ac6-8d5f-577800efcea8"}
     />
   </>
 );
