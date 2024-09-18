099f601a5024c19508eab3b750a7227471444a62
Antoine Kingue <contact@antoinek.fr>
2024-09-18 13:13:57 +0200

    📝 fix security headers in docs

--- docs/next.config.js
+++ docs/next.config.js
@@ -22,19 +22,21 @@ module.exports = withNextra(
     async headers() {
       return [
         {
-          key: "X-Frame-Options",
-          value: "SAMEORIGIN",
           source: "/(.*)",
-        },
-        {
-          key: "X-Content-Type-Options",
-          value: "nosniff",
-          source: "/(.*)",
-        },
-        {
-          key: "Referrer-Policy",
-          value: "origin-when-cross-origin",
-          source: "/(.*)",
+          headers: [
+            {
+              key: "X-Frame-Options",
+              value: "SAMEORIGIN",
+            },
+            {
+              key: "X-Content-Type-Options",
+              value: "nosniff",
+            },
+            {
+              key: "Referrer-Policy",
+              value: "origin-when-cross-origin",
+            },
+          ],
         },
       ];
     },
